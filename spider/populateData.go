5e11c847b5138931cff01066a238d928f5519e49
adrien pusel <adrien.pusel@gmail.com>
2024-03-16 21:20:25 +0100

    `batch` with `mutex` ok

--- spider/populateData.go
+++ spider/populateData.go
@@ -7,59 +7,66 @@ import (
 
 var extensions = []string{".jpg", ".jpeg", ".png", ".gif", ".bmp"}
 
-func PopulateData(data Data, links []string, allReadyVisited []string) Data {
+const batchSize = 30
+
+func PopulateData(data Data, links []string, allReadyVisited *[]string, allReadyHaveImage *[]string, mu *sync.Mutex) Data {
+	ch := make(chan struct{}, batchSize)
+	var wg sync.WaitGroup
+
+	fmt.Println("links", links)
 	for _, link := range links {
-		rawLinks, _ := GetLinksFromHTML(link)
-		// i can have multiple images with same name, i need to keep all and update their name.
-		images := FilterLinksByExtensions(rawLinks, extensions, true)
 
-		notImageLinks := FilterLinksByExtensions(rawLinks, extensions, false)
-		links = FilterNewLinks(notImageLinks, allReadyVisited)
+		wg.Add(1)
+		ch <- struct{}{}
 
-		data.Images = append(data.Images, images...)
-		data.Links = append(data.Links, links...)
+		go func(url string) {
+			defer func() {
+				fmt.Println("done", url)
+				wg.Done()
+			}()
+
+			fmt.Println("star", url)
+			rawLinks, err := GetLinksFromHTML(url)
+			if err != nil {
+				fmt.Println("bad url continue")
+				return
+			}
+			// i can have multiple images with same name, i need to keep all and update their name.
+			allImages := FilterLinksByExtensions(rawLinks, extensions, true)
+			notImageLinks := FilterLinksByExtensions(rawLinks, extensions, false)
+
+			mu.Lock()
+			images := FilterNewLinks(allImages, *allReadyHaveImage)
+			*allReadyHaveImage = append(*allReadyHaveImage, images...)
+
+			links = FilterNewLinks(notImageLinks, *allReadyVisited)
+			*allReadyVisited = append(*allReadyVisited, links...)
+			mu.Unlock()
+
+			data.Images = append(data.Images, images...)
+			data.Links = append(data.Links, links...)
+			<-ch
+		}(link)
 	}
+
+	wg.Wait()
 
 	return data
 }
 
 func LoopOnLinks(data map[int]Data, startUrl string, deep int) map[int]Data {
-	var wg sync.WaitGroup
+	var mu sync.Mutex
+
 	allReadyVisited := []string{startUrl}
-	batchSize := 10
-
-	// Create a channel to control the number of concurrent goroutines
-	semaphore := make(chan struct{}, batchSize)
-
+	allReadyHaveImage := []string{}
 	links := []string{startUrl}
-	for i := 0; i < deep; i++ {
-		// Acquire a slot in the semaphore
-		semaphore <- struct{}{}
-
-		// Increment the WaitGroup counter
-		wg.Add(1)
-
-		// Launch a goroutine for each iteration
-		go func(index int) {
-			defer func() {
-				// Signal the completion of the goroutine
-				wg.Done()
-
-				// Release the slot in the semaphore
-				<-semaphore
-				fmt.Println("done")
-			}()
-
-			fmt.Println("start loop", links, index, deep)
-			data[index] = Data{Images: make([]string, 0), Links: make([]string, 0)}
-			data[index] = PopulateData(data[index], links, allReadyVisited)
-			links = data[index].Links
-			allReadyVisited = append(allReadyVisited, links...)
-		}(i)
+	for index := 0; index < deep; index++ {
+		fmt.Println("start loop", links, index, deep)
+		data[index] = Data{Images: make([]string, 0), Links: make([]string, 0)}
+		data[index] = PopulateData(data[index], links, &allReadyVisited, &allReadyHaveImage, &mu)
+		links = data[index].Links
+		allReadyVisited = append(allReadyVisited, links...)
 	}
-
-	// Wait for all goroutines to finish
-	wg.Wait()
 
 	return data
 }
--- spider/spider.go
+++ spider/spider.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 const URL = "https://www.medical.fr/equipements-hospitaliers/chariot-hospitalier?page=2"
-const DEEP = 1
+
+// const URL = "https://dzone.com/articles/batch-processing-in-go"
+const DEEP = 3
 
 type Data struct {
 	Images []string
@@ -9,31 +17,56 @@ type Data struct {
 }
 
 func main() {
-	// check deep = 0
+	// !!test check deep = 0
 
-	//var data = make(map[int]Data, DEEP)
-	//data = LoopOnLinks(data, URL, DEEP)
-	//
-	//// Convert the data map to JSON
-	//jsonData, err := json.Marshal(data)
-	//if err != nil {
-	//	fmt.Println("Error converting to JSON:", err)
-	//	return
-	//}
-	//
-	//// Write the JSON data to a file
-	//err = os.WriteFile("data.json", jsonData, 0644)
-	//if err != nil {
-	//	fmt.Println("Error writing JSON file:", err)
-	//	return
-	//}
-	//
-	//fmt.Println("JSON data written to data.json")
+	var data = make(map[int]Data, DEEP)
+	data = LoopOnLinks(data, URL, DEEP)
 
-}
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, value := range s {
-		sum += value
+	// Convert the data map to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
 	}
+
+	// Write the JSON data to a file
+	err = os.WriteFile("data.json", jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+
+	fmt.Println("JSON data written to data.json")
 }
+
+//func fibonacci(n int, c chan int) {
+//	x, y := 0, 1
+//	for i := 0; i < n; i++ {
+//		c <- x
+//		x, y = y, x+y
+//	}
+//	close(c)
+//}
+//
+//func mainFibo() {
+//	c := make(chan int, 10)
+//	go fibonacci(cap(c), c)
+//}
+//
+//// ///////////////////////////////////////////////////////////////
+//func calculateAll() {
+//	s := []int{7, 6, 8, 1, 8, 8, 19}
+//	c := make(chan int)
+//	go sum(s[len(s)/2:], c)
+//	go sum(s[:len(s)/2], c)
+//	y := <-c + <-c
+//	fmt.Println(y)
+//}
+//
+//func sum(s []int, c chan int) {
+//	sum := 0
+//	for _, value := range s {
+//		sum += value
+//	}
+//	c <- sum
+//}
